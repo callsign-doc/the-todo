1f732eac5cafced0f42733e26eb737d1460a4f2a
Iman Haqim <imanhqm@protonmail.com>
2024-03-13 20:11:11 +0800

    Setup webpack bundling function before proceed

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import css from './style.css';
+
+console.log("you just reached the-todo hotline...")
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your HTML file
+      filename: 'index.html', // Output HTML file name
+    }),
+  ],
+};
